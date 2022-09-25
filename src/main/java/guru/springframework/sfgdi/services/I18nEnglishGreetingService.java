e313006165f491a846d517ca94511029dae18579
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2022-09-25 23:41:33 +0100

    last commit

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile({"EN","default"})
+@Service("i18nService")
+public class I18nEnglishGreetingService implements ProfileGreetingService{
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello world - EN";
+    }
+
+    @Override
+    public String getProfile() {
+        return "English";
+    }
+}
